8662ae4c04c76c475aa0cd64958762a727e1bda4
Jay Yang <hjyang1@uci.edu>
2018-04-11 15:43:09 -0700

    added layer to wall to bypasss single raycast return of player, ai working

---
+++ NotFlappyBird/Assets/Brain.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brain : MonoBehaviour {
+
+    int DNALength = 5;
+    public DNA dna;
+    public GameObject eyes;
+    bool seeDownWall = false;
+    bool seeUpWall = false;
+    bool seeBottom = false;
+    bool seeTop = false;
+
+    public LayerMask walls;
+
+    Vector3 startPosition;
+    public float timeAlive = 0;
+    public float distanceTravelled = 0;
+    public int crash = 0;
+    bool alive = true;
+
+    Rigidbody2D rb;
+
+    public void Init()
+    {
+        //init DNA
+        //0 forward
+        //1 upwall
+        //2 downwall
+        //3 normall upward
+
+        dna = new DNA(DNALength, 200);
+        this.transform.Translate(Random.Range(-1.5f, 1.5f), Random.Range(-1.5f, 1.5f), 0);
+        startPosition = this.transform.position;
+        rb = this.GetComponent<Rigidbody2D>();
+    
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Top" ||
+            collision.gameObject.tag == "Bottom" ||
+            collision.gameObject.tag == "UpWall" ||
+            collision.gameObject.tag == "DownWall")
+        {
+            crash++;
+        }
+        else if (collision.gameObject.tag == "Dead")
+        {
+            alive = false;
+        }
+
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!alive) return;
+
+        seeUpWall = false;
+        seeDownWall = false;
+        seeTop = false;
+        seeBottom = false;
+
+        RaycastHit2D hit = Physics2D.Raycast(eyes.transform.position, eyes.transform.forward, 1.0f, walls);
+
+        Debug.DrawRay(eyes.transform.position, eyes.transform.forward * 1.0f, Color.red);
+        Debug.DrawRay(eyes.transform.position, eyes.transform.up * 1.0f, Color.red);
+        Debug.DrawRay(eyes.transform.position, -eyes.transform.up * 1.0f, Color.red);
+
+        if (hit.collider != null)
+        {
+           
+            if(hit.collider.gameObject.tag == "UpWall")
+            {
+                seeUpWall = true;
+            }
+
+            else if (hit.collider.gameObject.tag == "DownWall")
+            {
+                seeDownWall = true;
+            }
+        }
+
+        hit = Physics2D.Raycast(eyes.transform.position, eyes.transform.up, 1.0f);
+        if(hit.collider != null)
+        {
+            if (hit.collider.gameObject.tag == "Top")
+            {
+                seeTop = true;
+            }
+
+        }
+        hit = Physics2D.Raycast(eyes.transform.position, -eyes.transform.up, 1.0f);
+        if (hit.collider != null)
+        {
+            if (hit.collider.gameObject.tag == "Bottom")
+            {
+                seeBottom = true;
+            }
+
+        }
+
+        timeAlive = PopulationManager.elapsed;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!alive) return;
+
+        float upForce = 0;
+        float forwardForce = 1.0f;
+
+        if (seeUpWall)
+        {
+            upForce = dna.GetGene(0);
+
+        }
+
+        else if (seeDownWall)
+        {
+            upForce = dna.GetGene(1);
+        }
+        else if (seeDownWall)
+        {
+            upForce = dna.GetGene(1);
+        }
+        else if (seeTop)
+        {
+            upForce = dna.GetGene(2);
+        }
+        else if (seeBottom)
+        {
+            upForce = dna.GetGene(3);
+        }
+        else
+        {
+            upForce = dna.GetGene(4);
+        }
+
+        rb.AddForce(this.transform.right * forwardForce);
+        rb.AddForce(this.transform.up * upForce * 0.1f);
+        distanceTravelled = this.transform.position.x - startPosition.x;
+    }
+
+
+}
---
+++ NotFlappyBird/Assets/PopulationManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PopulationManager : MonoBehaviour {
+
+    public GameObject botPrefab;
+    public GameObject startingPos;
+    public int populationSize = 50;
+
+    List<GameObject> population = new List<GameObject>();
+    public static float elapsed = 0;
+    public float trialTime = 5;
+    int generation = 1;
+
+    GUIStyle guiStyle = new GUIStyle();
+    private void OnGUI()
+    {
+        guiStyle.fontSize = 25;
+        guiStyle.normal.textColor = Color.white;
+        GUI.BeginGroup(new Rect(10, 10, 250, 150));
+        GUI.Box(new Rect(0, 0, 140, 140), "Stats", guiStyle);
+        GUI.Label(new Rect(10, 25, 200, 30), "Gen: " + generation, guiStyle);
+        GUI.Label(new Rect(10, 50, 200, 30), string.Format("Time: {0:0.00}", elapsed), guiStyle);
+        GUI.Label(new Rect(10, 75, 200, 30), "Population: " + population.Count, guiStyle);
+        GUI.EndGroup();
+
+    }
+
+    // Use this for initialization
+    void Start () {
+		for (int i = 0; i < populationSize; i++)
+        {
+            GameObject b = Instantiate(botPrefab, startingPos.transform.position, this.transform.rotation);
+            b.GetComponent<Brain>().Init();
+            population.Add(b);
+        }
+	}
+
+    GameObject Breed(GameObject par1, GameObject par2)
+    {
+        GameObject offSpring = Instantiate(botPrefab, startingPos.transform.position, this.transform.rotation);
+        Brain b = offSpring.GetComponent<Brain>();
+
+        b.Init();
+        b.dna.Combine(par1.GetComponent<Brain>().dna, par2.GetComponent<Brain>().dna);
+
+        return offSpring;
+    }
+
+    void BreedNewPopulation()
+    {
+        List<GameObject> sortedList = population.OrderBy(o => (o.GetComponent<Brain>().distanceTravelled)).ToList();
+
+        population.Clear();
+
+        for(int i = (int) (3 * sortedList.Count / 4.0f) -1; i < sortedList.Count - 1; i++)
+        {
+            population.Add(Breed(sortedList[i], sortedList[i + 1]));
+            population.Add(Breed(sortedList[i + 1], sortedList[i]));
+            population.Add(Breed(sortedList[i], sortedList[i + 1]));
+            population.Add(Breed(sortedList[i + 1], sortedList[i]));
+        }
+
+        for(int i = 0; i < sortedList.Count; i++)
+        {
+            Destroy(sortedList[i]);
+
+        }
+        generation++;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        elapsed += Time.deltaTime;
+        if(elapsed >= trialTime)
+        {
+            BreedNewPopulation();
+            elapsed = 0;
+        }
+	}
+}
